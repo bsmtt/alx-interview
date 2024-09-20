c19cc3308a55e1e58b0984efd71c5d4d32afe230
bsm2 <basmatalaat2@gmail.com>
2024-09-21 02:27:41 +0300

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,40 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-const args = process.argv;
-const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+const arg = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${arg}`;
 
-/**
- * Wrapper function for request object that allows it
- * to work with async and await
- * @param   {String} url - site url
- * @returns {Promise}    - promise object that resolves
- *                         with parsed JSON response
- *                         and rejects with the request error.
- */
-function sendRequest (url) {
-    return new Promise((resolve, reject) => {
-      request.get(url, (error, response, body) => {
-        if (error) reject(error);
-        else resolve(JSON.parse(body));
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  let characters = JSON.parse(body).characters
+  for (const characterId of characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
       });
     });
   }
-  
-/**
- * Entry point - retrieves movie character info from Star Wars API
- * based on the movie ID passed as a CLI parameter.
- * Prints the names of the characters in the order of appearance.
- */
-  async function main () {
-    if (args.length < 3) return;
-
-    const movie = await sendRequest(movieUrl);
-    if (movie.characters === undefined) return;
-    for (const characterUrl of movie.characters) {
-      const character = await sendRequest(characterUrl);
-      console.log(character.name);
-    }
-  }
-  
-  main();
+});
