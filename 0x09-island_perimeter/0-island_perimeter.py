f769cbad39db293ffafcc4f42eb07d88f6e7fef6
bsm2 <basmatalaat2@gmail.com>
2024-10-10 21:24:47 +0300

    island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,23 +7,20 @@ def island_perimeter(grid):
     """Return the perimiter of an island.
 
     Args:
-        grid (list): list of a list 
+        grid (list): list of a list
 
     Returns:
         int: perimiter of an island
     """
-    height, width, size = len(grid), len(grid[0]), 0
-    
-    for x in range(height):
-        for y in range (width):
-            if grid[x][y] == 1:
-                if y > 0 and grid[x][y - 1] == 0:
-                    size += 1
-                if x > 0 and grid[x - 1][y] == 0:
-                    size += 1
-                if grid[x][y + 1] == 0:
-                    size += 1
-                if grid[x + 1][y] == 0:
-                    size += 1
+    height, width, size, edges = len(grid), len(grid[0]), 0, 0
 
-    return size
+    for x in range(height):
+        for y in range(width):
+            if grid[x][y] == 1:
+                size += 1
+                if (y > 0 and grid[x][y - 1] == 1):
+                    edges += 1
+                if (x > 0 and grid[x - 1][y] == 1):
+                    edges += 1
+
+    return size * 4 - edges * 2
